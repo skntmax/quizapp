293b3b9938cd5ed1ca5581af8804a715372ec2cf
Shubham Dhiman <shubham@valueinnovationlabs.com>
2024-10-12 09:13:09 +0530

    fix the button in mobile srreen

--- src/modules/common_modules/Footer.jsx
+++ src/modules/common_modules/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <>
-            <footer className="sticky top-5 z-40 w-full text-center bg-background">
+            <footer className="sticky top-5 z-40  w-full text-center bg-background">
                 © 2021 Copyright: bytecode.live
             </footer>
         </>
