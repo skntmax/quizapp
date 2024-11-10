38e620c675eb8f427370d3f8d66730c31bf8ae58
Shubham Dhiman <shubham@valueinnovationlabs.com>
2024-11-10 17:07:29 +0530

    add loader in diffcult level click

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -1,8 +1,40 @@
+'use client'
+import { cookies } from "@/constant";
+import { getRequest } from "@/crud_operations/RequestHandler";
 import CommonHeader from "@/modules/common_modules/CommonHeader";
 import Footer from "@/modules/common_modules/Footer";
 import QuizPage from "@/modules/ui_modules/QuizPage";
+import { getCookie } from "cookies-next";
+import { useEffect, useState } from "react";
+
+const customHeader = {
+    headers: {
+        "Authorization": `Bearer ${getCookie(cookies.btcode_live_cd_key)}`, // Replace with your actual token or header value
+        // Add any other custom headers here
+    },
+}
 
 export default function Page() {
+    const [data, setData] = useState(undefined);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await getRequest("quiz/get-users-quiz-history",
+                customHeader
+            );
+            if (!response.status) {
+                throw new Error({ success: false, message: "Failed to fetch data" });
+            } else {
+                setData(response.result)
+            }
+        } catch (err) {
+            throw new Error(err.message); // Set any errors that occur
+        }
+    };
     return <>
         <CommonHeader />
         <QuizPage />
--- src/modules/common_modules/DialogUi.jsx
+++ src/modules/common_modules/DialogUi.jsx
@@ -1,19 +1,17 @@
 'use client';
+import { Button } from "@/components/ui/button";
 import {
     Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle
+    DialogContent
 } from "@/components/ui/dialog";
-import { Stepper } from 'react-form-stepper';
-import { SelectUi } from "./SelectUi";
-import { ShimmerButton } from "react-shimmer-effects";
-import { ShimmerButtonThree, ShimmerButtonTen } from "./shimmer-effects/ShimmerButtonUi";
-import { Button } from "@/components/ui/button";
 import { getRandomVariant } from "@/utils/logix";
+import { Stepper } from 'react-form-stepper';
+import { InfinitySpin } from "react-loader-spinner";
+import { ShimmerButtonTen, ShimmerButtonThree } from "./shimmer-effects/ShimmerButtonUi";
+import { useState } from "react";
+import { Clock } from "lucide-react";
 
-export function DialogUi({ data, setData, handleCategories, handleDefficultLevel, steps, handleMoreCategory }) {
+export function DialogUi({ data, setData, handleCategories, handleDefficultLevel, steps, handleMoreCategory, loder }) {
     const { categories, difficulty_level, more_cat_loder } = data;
 
     return (
@@ -24,18 +22,29 @@ export function DialogUi({ data, setData, handleCategories, handleDefficultLevel
                     steps={steps}
                     activeStep={data.activeStep}
                 />
-
                 {
-                    data.activeStep == 0 && (
+                    loder ? <div className="flex flex-wrap justify-center gap-4 p-4">
+                        <InfinitySpin
+                            visible={true}
+                            width="200"
+                            color="#ed1d24"
+                            ariaLabel="infinity-spin-loading"
+                        />
+                    </div>
+                        :
                         <>
-                            {/* <DialogHeader>
+
+                            {
+                                data.activeStep == 0 && (
+                                    <>
+                                        {/* <DialogHeader>
                                 <DialogTitle>Select language preference</DialogTitle>
                                 <DialogDescription>
                                     Choose your preferences from the available options. Your selection will help us tailor your experience to better suit your needs. Make sure to review and save your changes before proceeding.
                                 </DialogDescription>
                             </DialogHeader> */}
 
-                            {/* <div className="grid gap-4 py-4">
+                                        {/* <div className="grid gap-4 py-4">
                                 <div className="grid grid-cols-4 items-center gap-4">
                                     <SelectUi />
                                 </div>
@@ -43,80 +52,90 @@ export function DialogUi({ data, setData, handleCategories, handleDefficultLevel
                                     <SelectUi />
                                 </div>
                             </div> */}
-                            {
-                                categories.data === undefined &&
-                                (
-                                    <div className="flex flex-wrap justify-center gap-4 p-4">
-                                        <ShimmerButtonTen className='w-[10px]' />
-                                    </div>)
+                                        {
+                                            categories.data === undefined &&
+                                            (
+                                                <div className="flex flex-wrap justify-center gap-4 p-4">
+                                                    <ShimmerButtonTen className='w-[10px]' />
+                                                </div>)
+                                        }
+                                        {
+                                            more_cat_loder &&
+                                            (
+                                                <div className="flex flex-wrap justify-center gap-4 p-4">
+                                                    <ShimmerButtonTen className='' />
+                                                </div>)
+                                        }
+                                        {
+                                            categories.data !== undefined && categories.data.length > 0 && !more_cat_loder &&
+                                            (
+                                                <div className="flex flex-wrap justify-center gap-4 p-4">
+                                                    {categories.data.map((item) =>
+                                                        <Button
+                                                            variant={getRandomVariant()}
+                                                            size='lg'
+                                                            onClick={() => handleCategories(item._id)}
+                                                        >
+                                                            {`${item.TITLE} - ${item.totalCount}`}
+                                                        </Button>
+                                                    )}
+                                                    <Button
+                                                        variant={getRandomVariant()}
+                                                        size='lg'
+                                                        onClick={handleMoreCategory}
+                                                    >
+                                                        more...
+                                                    </Button>
+                                                </div>)
+                                        }
+                                    </>
+                                )
                             }
-                            {
-                                more_cat_loder  &&
-                                (
-                                    <div className="flex flex-wrap justify-center gap-4 p-4">
-                                        <ShimmerButtonTen className='' />
-                                    </div>)
-                            }
-                            {
-                                categories.data !== undefined && categories.data.length > 0 && !more_cat_loder &&
-                                (
-                                    <div className="flex flex-wrap justify-center gap-4 p-4">
-                                        {categories.data.map((item) =>
-                                            <Button
-                                                variant={getRandomVariant()}
-                                                size='lg'
-                                                onClick={() => handleCategories(item._id)}
-                                            >
-                                                {item.TITLE}
-                                            </Button>
-                                        )}
-                                        <Button
-                                            variant={getRandomVariant()}
-                                            size='lg'
-                                            onClick={handleMoreCategory}
-                                        >
-                                            more...
-                                        </Button>
-                                    </div>)
-                            }
-                        </>
-                    )
-                }
 
-                {
-                    data.activeStep == 1 && (
-                        <>
-                            {/* <DialogHeader>
+                            {
+                                data.activeStep == 1 && (
+                                    <>
+                                        {/* <DialogHeader>
                                 <DialogTitle>Select catogo preference</DialogTitle>
                                 <DialogDescription>
                                     Choose your preferences from the available options. Your selection will help us tailor your experience to better suit your needs. Make sure to review and save your changes before proceeding.
                                 </DialogDescription>
                             </DialogHeader> */}
-                            {
-                                difficulty_level.data === undefined &&
-                                (
-                                    <div className="flex flex-wrap justify-center gap-4 p-4">
-                                        <ShimmerButtonThree className='' />
-                                    </div>)
-                            }
-                            {
-                                difficulty_level.data !== undefined && difficulty_level.data.length > 0 &&
-                                (
-                                    <div className="flex flex-wrap justify-center gap-4 p-4">
-                                        {difficulty_level.data.map((item) =>
-                                            <Button
-                                                variant={getRandomVariant()}
-                                                size='lg'
-                                                onClick={() => handleDefficultLevel(item._id)}
-                                            >
-                                                {item.DIFFICULTY_LEVEL}
-                                            </Button>
-                                        )}
-                                    </div>)
+                                        {
+                                            difficulty_level.data === undefined &&
+                                            (
+                                                <div className="flex flex-wrap justify-center gap-4 p-4">
+                                                    <ShimmerButtonThree className='' />
+                                                </div>)
+                                        }
+                                        {
+                                            difficulty_level.data !== undefined && difficulty_level.data.length > 0 &&
+                                            (
+                                                <div className="flex flex-wrap justify-center gap-4 p-4">
+                                                    {difficulty_level.data.map((item) =>
+                                                        <Button
+                                                            variant={getRandomVariant()}
+                                                            size="lg"
+                                                            onClick={() => handleDefficultLevel(item._id)}
+                                                        >
+                                                            <div className="flex flex-col items-center">
+                                                                <div className="text-lg font-semibold">
+                                                                    {`${item.DIFFICULTY_LEVEL} ${item.TOTAL_COUNT}`}
+                                                                </div>
+                                                                <div className="flex items-center space-x-2 mt-1">
+                                                                    <span>{'10:20:00'}</span>
+                                                                    <Clock className="w-5 h-5" /> {/* Adjust size as needed */}
+                                                                </div>
+                                                            </div>
+                                                        </Button>
+                                                    )}
+                                                </div>)
+                                        }
+                                    </>
+
+                                )
                             }
                         </>
-
-                    )
                 }
             </DialogContent>
         </Dialog>
