7c898ef70fe24aae32142acbf6f76aa17c87e13f
Shubham Dhiman <shubham@valueinnovationlabs.com>
2024-11-10 17:17:56 +0530

    uncmt middleware code

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 import { bytecodeUrls, cookies } from './constant';
 export function middleware(request) {
-  // // Check if a specific cookie exists
-  // const myCookie = request.cookies.get(cookies.btcode_live_cd); // Replace 'myCookieName' with your cookie name
+  // Check if a specific cookie exists
+  const myCookie = request.cookies.get(cookies.btcode_live_cd); // Replace 'myCookieName' with your cookie name
 
-  // // If the cookie does not exist, redirect to bytecode.live
-  // if (!myCookie) {
-  //   return NextResponse.redirect(new URL(bytecodeUrls.authenticatedFalse, request.url));
-  // }
+  // If the cookie does not exist, redirect to bytecode.live
+  if (!myCookie) {
+    return NextResponse.redirect(new URL(bytecodeUrls.authenticatedFalse, request.url));
+  }
 
   // If the cookie exists, proceed with other logic or return the original response
   return NextResponse.next(); // Allow the request to continue
