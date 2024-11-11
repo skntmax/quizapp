c63bd678b9220230fbd39bf33a80f3e3270a904c
shashi kant <skntjee@gmail.com>
2024-11-11 23:59:05 +0530

    added

--- src/modules/common_modules/DialogUi.jsx
+++ src/modules/common_modules/DialogUi.jsx
@@ -123,7 +123,7 @@ export function DialogUi({ data, setData, handleCategories, handleDefficultLevel
                                                                     {`${item.DIFFICULTY_LEVEL} ${item.TOTAL_COUNT}`}
                                                                 </div>
                                                                 <div className="flex items-center space-x-2 mt-1">
-                                                                    <span>{'10:20:00'}</span>
+                                                                    <span>{item.QUIZ_TIMESPAN}</span>
                                                                     <Clock className="w-5 h-5" /> {/* Adjust size as needed */}
                                                                 </div>
                                                             </div>
