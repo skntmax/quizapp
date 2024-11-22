d635086cc40993d3f1fc426c663c2537f550e0e5
Shubham Dhiman <shubham@valueinnovationlabs.com>
2024-11-22 18:27:29 +0530

    _

--- src/modules/common_modules/CommonHeader.jsx
+++ src/modules/common_modules/CommonHeader.jsx
@@ -12,6 +12,7 @@ export default function CommonHeader() {
     const cdDetails = getSingleCookiesCSR(cookies.btcode_live_cd)
     let quizDetails = useSelector(ele => ele?.quiz?.quizSessionDetails)
     const lastSentReward = useSelector((state) => state.quiz.lastSentReward);
+    const dialog = useSelector((state) => state.quiz.dialog);
 
     let TimerCmp = useCallback(() => {
         return <Timer timerSession={quizDetails} />
@@ -24,17 +25,17 @@ export default function CommonHeader() {
                     <div className="flex items-center text-black dark:text-white space-x-4">
                         {/* <Icons.logo className="h-6 w-6" /> */}
                         <a href="https://bytecode.live/">
-                        <span className="inline-block font-bold ">
-                        <img  width="170" height="25" 
-                         src="https://bytecode.live/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flightlogo.178ba7a4.png&w=256&q=75" 
-                         style={{color: "transparent"}} />
-                        </span>
+                            <span className="inline-block font-bold ">
+                                <img width="170" height="25"
+                                    src="https://bytecode.live/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flightlogo.178ba7a4.png&w=256&q=75"
+                                    style={{ color: "transparent" }} />
+                            </span>
                         </a>
                     </div>
                 </div>
                 <div className="flex flex-1 items-center justify-end space-x-4">
                     <nav className="flex items-center space-x-1">
-                        {quizDetails?.QUIZ_TIMESPAN && quizDetails?.CREATED_ON && TimerCmp()}
+                        {!dialog && quizDetails?.QUIZ_TIMESPAN && quizDetails?.CREATED_ON && TimerCmp()}
                         <User /> {cdDetails ? cdDetails.USERNAME : null}
                         <Trophy className="ml-2" />
                         {lastSentReward}
